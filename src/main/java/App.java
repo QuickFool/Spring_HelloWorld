0adc739f2ac146fe2b3236d9cdf28c3493f3b533
QuickFool <olvvelold@mail.ru>
2024-08-18 14:23:50 +0300

    TaskSpring 2.1.2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,20 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+
+        Cat beanCat = (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat.getMessage());
+
+        Cat beanCat1 = (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat1.getMessage());
+
+        boolean comparisonHW = bean == bean1;
+        System.out.println(comparisonHW);
+        boolean comparisonCat = beanCat == beanCat1;
+        System.out.println(comparisonCat);
+
     }
 }
